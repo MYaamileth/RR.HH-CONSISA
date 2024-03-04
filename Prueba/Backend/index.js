faf430ef31e1103e992a8dc5f6241ba48ea4b338
1Yamileth <marilynyamilethmejia@gmail.com>
2024-03-04 12:40:28 -0600

    Mantenimiento usuario
    Trae listado de información y puedo mandar información  a l base de datos.

--- Prueba/Backend/index.js
+++ Prueba/Backend/index.js
@@ -3,17 +3,19 @@ import mysql from "mysql";
 import cors from "cors";
 
 const app = express();
+app.use(express.json());
+app.use(cors());
 
 // Conexión a la base de datos
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "1234proyecto_",
-  database: "consisa",
+  database: "prueba",
 });
 
 
-// Manejo de eventos de conexión y errores al momento de conectar la base de datos.
+// Manejo de eventos de conexión a la base de datos, backend y errores al momento de conectar la base de datos.
 db.connect((err) => {
   if (err) {
     console.error('Error al conectar a la base de datos:', err);
@@ -26,6 +28,9 @@ db.on('error', (err) => {
   console.error('Error en la conexión a la base de datos:', err);
 });
 
+app.listen(8800, () => {
+  console.log("Conectado al backend en el puerto 8800");
+});
 
 
 // Si tienen problemas de autenticación con la base de datos, usen el siguiente código en su línea de comandos:
@@ -37,8 +42,65 @@ app.get("/", (req, res) => {
 });
 
 
-app.use(express.json());
-app.use(cors());
+
+// BD DE PRUEBA, LA RUTA DEBE DE PASAR AL SIGUIENTE APP.POST
+app.post("/creacionUsuario", (req, res) => {
+  //DESPUES DEL IGUAL TIENE QUE IR IGUAL A COMO ESTA ESCRITO EL CAMPO EN LA BASE DE DATOS
+    const Nombre_empleado= req.body.Nombre_empleado;
+    const Correo = req.body.Correo;
+    const query = `INSERT INTO tbl_empleado (Nombre_empleado, Correo) VALUES (?,?)`;
+
+      db.query(query, [Nombre_empleado, Correo], (err, data) => {
+      if (err) return res.json(err);
+      return res.json("USUARIO INSERTADO CORRECTAMENTE");
+    });
+  });
+
+
+// BD DE PRUEBA, LA RUTA DEBE DE PASAR AL SIGUIENTE APP.get
+app.get("/traerUsuarios", (req, res) => {
+  const query = 'SELECT * FROM tbl_empleado';
+  db.query(query, (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
+  });
+});
+
+
+
+
+
+
+// Mandar datos del empleado a la tabla tbl_ms_usuario.  AQUI DEBE DE IR LA RUTA DE  creacionUsuario
+app.post("/", (req, res) => {
+  //DESPUES DEL IGUAL TIENE QUE IR IGUAL A COMO ESTA ESCRITO EL CAMPO EN LA BASE DE DATOS
+    const Id_estado= req.body.Id_estado;
+    const Id_rol = req.body.Id_rol;
+    const Id_puesto= req.body.Id_puesto;
+    const Usuario= req.body.Usuario;
+    const Nombre_Completo_Usuario= req.body.Nombre_Completo_Usuario;
+    const Contraseña= req.body.Contraseña;
+    const Fecha_Ultima_Conexion= req.body.Fecha_Ultima_Conexion;
+    const Primer_ingreso= req.body.Primer_ingreso;
+    const Correo_electronico= req.body.Correo_electronico;
+    const Fecha_vencimiento= req.body.Fecha_vencimiento;
+    //  const token= req.body.Token;
+    const Creado_por= req.body.Creado_por;
+    const Modificado_por= req.body.Modificado_por;
+    const Fecha_creacion= req.body.Fecha_creacion;
+    const Fecha_modificacion= req.body.Fecha_modificacion;
+
+    const query = `INSERT INTO tbl_ms_usuario (Id_estado, Id_rol, Id_puesto,Usuario, Nombre_Completo_Usuario, Contraseña,Fecha_Ultima_Conexion,Primer_ingreso,Correo_electronico,
+      Fecha_vencimiento,Creado_por,Modificado_por,Fecha_creacion,Fecha_modificacion) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)`;
+
+      db.query(query, [Id_estado, Id_rol, Id_puesto, Usuario, Nombre_Completo_Usuario, Contraseña, Fecha_Ultima_Conexion, Primer_ingreso,Correo_electronico,Fecha_vencimiento, 
+        Creado_por, Modificado_por,Fecha_creacion, Fecha_modificacion], (err, data) => {
+      if (err) return res.json(err);
+      return res.json("USUARIO INSERTADO CORRECTAMENTE");
+    });
+  });
+
+
 // Traer datos de la tabla tbl_ms_usuario
 app.get("/Usuario", (req, res) => {
   const query = 'SELECT * FROM tbl_ms_usuario';
@@ -48,39 +110,11 @@ app.get("/Usuario", (req, res) => {
   });
 });
 
-// Mandar datos del empleado a la tabla tbl_ms_usuario
-app.post("/tbl_ms_usuario", (req, res) => {
-    //¿VAN LAS LLAVES PRIMARIAS?
-    //DESPUES DEL IGUAL TIENE QUE IR IGUAL A COMO ESTA ESCRITO EL CAMPO EN LA BASE DE DATOS
-  const usuario= req.body.Usuario;
-  const nombreCompletoUsuario= req.body.Nombre_Completo_Usuario;
-  const contraseña= req.body.Contraseña;
-  const fecha_Ultima_Conexion= req.body.Fecha_Ultima_Conexion;
-  const primer_Ingreso= req.body.Primer_ingreso;
-  const correo_Electronico= req.body.Correo_electronico;
-  const fecha_Vencimiento= req.body.Fecha_vencimiento;
-//  const token= req.body.Token;
-  const creado_Por= req.body.Creado_por;
-  const modificado_Por= req.body.Modificado_por;
-  const fecha_Creacion= req.body.Fecha_creacion;
-  const fecha_Modificacion= req.body.Fecha_modificacion;
- 
-  const query = `INSERT INTO tbl_ms_usuario (Usuario, Nombre_Completo_Usuario, Contraseña,Fecha_Ultima_Conexion,Primer_ingreso,Correo_electronico,
-    Fecha_vencimiento,Token,Creado_por,Modificado_por,Fecha_creacion,Fecha_modificacion) VALUES (?,?,?,?,?,?,?,?,?,?,?,?)`;
-
-    db.query(query, [usuario, nombreCompletoUsuario,contraseña,fecha_Ultima_Conexion,primer_Ingreso,correo_Electronico,fecha_Vencimiento, 
-      creado_Por, modificado_Por,fecha_Creacion, fecha_Modificacion], (err, data) => {
-    if (err) return res.json(err);
-    return res.json("SE PUEDE MANDAR CORRECTAMENTE");
-  });
-});
-
-
 /*Actualizar usuario,recibe de parametro el id del usuario.*/
 app.put("/tbl_ms_usuario/:id", (req, res) => {
   const id = req.params.id; // Obtiene el ID del usuario a modificar de la ruta
 
-  const usuario = req.body.Usuario;
+  const nombreUsuario = req.body.Usuario;
   const nombreCompletoUsuario = req.body.Nombre_Completo_Usuario;
   const contraseña = req.body.Contraseña;
   const fecha_Ultima_Conexion = req.body.Fecha_Ultima_Conexion;
@@ -148,7 +182,3 @@ app.delete("/tbl_ms_usuario/:id", (req, res) => {
 
 
 
-
-app.listen(8800, () => {
-  console.log("Conectado al backend");
-});
--- Prueba/sisrr.hh/src/Componentes/Pages/MantenimientoUsuario.js
+++ Prueba/sisrr.hh/src/Componentes/Pages/MantenimientoUsuario.js
@@ -1,128 +1,220 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { Link } from 'react-router-dom';
-import './mantenimientoUsuario.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import "./mantenimientoUsuario.css";
 
 const MantenimientoUsuario = () => {
-    const [cargando, setCargando] = useState(false);
-    const [usuarios, setUsuarios] = useState([]);
+  const [cargando, setCargando] = useState(false);
+  /*const [usuarios, setUsuarios] = useState([]);
 
-    useEffect(() => {
-        const fetchAllUsuarios = async () => {
-            try {
-                const res = await axios.get('http://localhost:8800/Usuario');
-                setUsuarios(res.data);
-            } catch (error) {
-                console.log(error);
-            }
-        };
+  useEffect(() => {
+    const fetchAllUsuarios = async () => {
+      try {
+        const res = await axios.get("http://localhost:8800/Usuario");
+        setUsuarios(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
 
-        fetchAllUsuarios();
-    }, []);
+    fetchAllUsuarios();
+  }, []);*/
 
-    return (
-        <div data-maintenance-usuario="true" className="maintenance-container">
-            {cargando ? (
-                <div className="loading">Cargando...</div>
-            ) : (
-                <>
-                    <main>
-                        <section className="formulario">
-                            <div className="form-container">
-                              <h1>MANTENIMIENTO DE USUARIOS</h1>
-                              <p></p>
-                                <div className="input-container">
-                                    <input type="text" placeholder="ID Usuario" className="textbox" />
-                                    <input type="text" placeholder="Usuario" className="textbox" />
-                                    <input type="text" placeholder="Nombre Completo" className="textbox" />
-                                </div>
+  // const res = await axios.get("http://localhost:8800/Usuario")
 
-                                <div className="input-container">
-                                    <div className="flex-row">
-                                        <input type="text" placeholder="Contraseña" className="textbox" />
-                                        <input type="text" placeholder="Email" className="textbox" />
-                                        <div className="checkbox-container">
-                                            <div className="estado-container">
-                                                <label htmlFor="estado_usuario">Estado: </label>
-                                                <input type="checkbox" id="estado_usuario" className="input" />
-                                            </div>
-                                            <span className="estado-label">Activo</span>
-                                        </div>
-                                    </div>
-                                </div>
+  // Variable de estado para indicar si se está cargando la información
+  const [idusuario, setIdUsuario] = useState("");
+  const [estado, setEstado] = useState("");
+  const [roles, setRoles] = useState("");
+  const [puesto, setPuesto] = useState("");
+  const [usuario, setUsuario] = useState("");
+  const [nombreCompletoUsuario, setNombreCompletoUsuario] = useState("");
+  const [contraseña, setContraseña] = useState("");
+  const [fecha_Ultima_Conexion, setFecha_Ultima_Conexion] = useState("");
+  const [primer_Ingreso, setPimerIngreso] = useState("");
+  const [correo_Electronico, setCorreo_Electronico] = useState("");
+  const [fecha_Vencimiento, setFecha_Vencimiento] = useState("");
+  const [creado_Por, setCreado_Por] = useState("");
+  const [modificado_Por, setModificado_Por] = useState("");
+  const [fecha_Creacion, setFecha_Creacion] = useState("");
+  const [fecha_Modificacion, setFecha_Modificacion] = useState("");
+  
+ //CAMPOS PARA CARGAR LA INFORMACION DE PRUEBA(DESPUES SE ELIMINARAN)
+  const [Nombre_empleado, setNombre_empleado] = useState("");
+  const [Correo, setCorreo] = useState("");
 
-                                <div className="input-container">
-                                    <div className="flex-row">
-                                            <label>Puesto:
-                                              <select className="inputPuesto">
-                                                  <option value="administrador">Gerente de IT</option>
-                                                  <option value="usuario">Administrador de BD</option>
-                                                  <option value="agregarRol">Agregar Rol</option>
-                                              </select>
-                                            </label>
+  const [usuarioLista, setLista]=useState([]);//Lita para trer usuarios
 
-                                            <label>Rol:
-                                              <select className="inputRol">
-                                                  <option value="administrador">Administrador</option>
-                                                  <option value="usuario">Usuario</option>
-                                                  <option value="agregarRol">Agregar Rol</option>
-                                              </select>
-                                            </label>
-                                    </div>
-                                </div>
 
-                                <div className="form-buttons">
-                                    <button className="submit">
-                                        Crear
-                                    </button>
-                                    <button className="submit">
-                                        Editar
-                                    </button>
-                                    <button className="submit">
-                                        Eliminar
-                                    </button>
-                                    <button className="submit">
-                                        Revisar
-                                    </button>
-                                </div>
-                            </div>
-                        </section>
 
-                        <section className="tabla-usuarios">
-                            <table className="table table-striped table-hover">
-                                <thead>
-                                    <tr>
-                                    <th>ID Usuario</th>
-                                    <th>Usuario</th>
-                                    <th>Nombre Completo de Usuario</th>
-                                    <th>Contraseña</th>
-                                    <th>Fecha Vencimiento De Contraseña</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    {usuarios.map((usuario) => (
-                                        <tr key={usuario.id_usuario}>
-                                            <td>{usuario.usuario}</td>
-                                            <td>{usuario.nombreCompletoUsuario}</td>
-                                            <td>{usuario.contrasena}</td>
-                                            <td>{usuario.fecha_Ultima_Conexion}</td>
-                                            <td>{usuario.primer_Ingreso}</td>
-                                            <td>{usuario.correo_Electronico}</td>
-                                            <td>{usuario.fecha_Vencimiento}</td>
-                                            <td>{usuario.creado_Por}</td>
-                                            <td>{usuario.fecha_Creacion}</td>
-                                            <td>{usuario.modificado_Por}</td>
-                                            <td>{usuario.fecha_Modificacion}</td>
-                                        </tr>
-                                    ))}
-                                </tbody>
-                            </table>
-                        </section>
-                    </main>
-                </>
-            )}
-        </div>
-    );
+  //EMPIEZA CRUD
+
+  //Envio de valores capturados por los campos del frontend
+  const creacionUsuario =()=>{
+    axios.post("http://localhost:8800/creacionUsuario", {
+      Nombre_empleado: Nombre_empleado,
+      Correo: Correo,
+
+      //LOS CAMPOS ANTERIORES DESPUES DE DEBEN ELIMINAR DESPUES
+      Id_Estado: estado,
+      Id_rol: roles,
+      Id_puesto: puesto,
+      Usuario: usuario,
+      Nombre_Completo_Usuario: nombreCompletoUsuario,
+      Contraseña: contraseña,
+      Fecha_Ultima_Conexion: fecha_Ultima_Conexion,
+      Primer_ingreso: primer_Ingreso,
+      Correo_electronico: correo_Electronico,
+      Fecha_vencimiento: fecha_Vencimiento,
+      Creado_por: creado_Por,
+      Modificado_por: modificado_Por,
+      Fecha_creacion: fecha_Creacion,
+      Fecha_modificacion: fecha_Modificacion
+    }). then(()=> {
+      traerUsuarios(); //cada que se guarde un usuario se enlistara en la tabla
+      alert("Usuario registrado")
+    });
+  }
+
+  //Trae valores capturados por los campos del frontend
+  const traerUsuarios =()=>{
+    axios.get("http://localhost:8800/traerUsuarios"). then((response)=> {
+      setLista(response.data);//VENDRAN TODOS LOS DATOS DESDE LA API
+    });
+  }
+
+  traerUsuarios(); //Desde que se ingresa al apartado, traera los usuarios
+
+//EN LOS CAMPOS DE USUARIO Y EMAIL, CA,BIAR EL SET UNA VEZ QUE FUNIONE, PUES ESTAN LOS DE PRUEBA
+  return (
+    <div data-maintenance-usuario="true" className="maintenance-container">
+      {cargando ? (
+        <div className="loading">Cargando...</div>
+      ) : (
+        <>
+          <main>
+            <section className="formulario">
+              <div className="form-container">
+                <h1>MANTENIMIENTO DE USUARIOS</h1>
+                <p></p>
+                <div className="input-container">
+                  <input 
+                    type="text"
+                    placeholder="ID Usuario"
+                    className="textbox"
+                  />
+                  <input
+                    onChange={(event) =>{//el "event" guada lo que se ingreso en el campo
+                      setNombre_empleado(event.target.value); //con el "target" nosotros estamos diciendo que queremos el valor y se lo asignamo a "setusuario"
+                    }}
+                    type="text"
+                    placeholder="Usuario"
+                    className="textbox"
+                  />
+                  <input
+                    onChange={(event) =>{//el "event" guada lo que se ingreso en el campo
+                      setNombreCompletoUsuario(event.target.value); //con el "target" nosotros estamos diciendo que queremos el valor y se lo asignamo a "setusuario"
+                    }}
+                    type="text"
+                    placeholder="Nombre Completo"
+                    className="textbox"
+                  />
+                </div>
+
+                <div className="input-container">
+                  <div className="flex-row">
+                    <input
+                      onChange={(event) =>{//el "event" guada lo que se ingreso en el campo
+                        setContraseña(event.target.value); //con el "target" nosotros estamos diciendo que queremos el valor y se lo asignamo a "setusuario"
+                      }}
+                      type="text"
+                      placeholder="Contraseña"
+                      className="textbox"
+                    />
+                    <input
+                      onChange={(event) =>{//el "event" guada lo que se ingreso en el campo
+                        setCorreo(event.target.value); //con el "target" nosotros estamos diciendo que queremos el valor y se lo asignamo a "setusuario"
+                      }}
+                      type="text"
+                      placeholder="Email"
+                      className="textbox"
+                    />
+                    <div className="checkbox-container">
+                      <div className="estado-container">
+                        <label htmlFor="estado_usuario">Estado: </label>
+                        <input 
+                          onChange={(event) =>{//el "event" guada lo que se ingreso en el campo
+                            setEstado(event.target.value); //con el "target" nosotros estamos diciendo que queremos el valor y se lo asignamo a "setusuario"
+                          }}
+                          type="checkbox"
+                          id="estado_usuario"
+                          className="input"
+                        />
+                      </div>
+                      <span className="estado-label">Activo</span>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="input-container">
+                  <div className="flex-row">
+                    <label>
+                      Puesto:
+                      <select className="inputPuesto"  onChange={(event) => setPuesto(event.target.value)}>
+                        <option value="administrador">Gerente de IT</option>
+                        <option value="usuario">Administrador de BD</option>
+                        <option value="agregarRol">Agregar Rol</option>
+                      </select>
+                    </label>
+
+                    <label>
+                      Rol:
+                      <select className="inputRol " onChange={(event) => setRoles(event.target.value)} >
+                        <option value="administrador">Administrador</option>
+                        <option value="usuario">Usuario</option>
+                        <option value="agregarRol">Agregar Rol</option>
+                      </select>
+                    </label>
+                  </div>
+                </div>
+  
+                <div className="form-buttons">
+                  <button onClick={creacionUsuario} className="submit">Crear</button>
+                  <button className="submit">Editar</button>
+                  <button className="submit">Eliminar</button>
+                  <button className="submit">Revisar</button>
+                </div>
+              </div>
+            </section>
+            <section className="tabla-usuarios">
+              <table className="table table-striped table-hover">
+                <thead>
+                  <tr>
+                    <th>ID Usuario</th>
+                    <th>Usuario</th>
+                    <th>Nombre Completo de Usuario</th>
+                    <th>Contraseña</th>
+                    <th>Fecha Vencimiento De Contraseña</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {usuarioLista.map((val, key) => {
+                    return   <tr>
+                            <th>{val.Correo}</th>
+                            <th>{val.Nombre_empleado}</th>
+                            </tr>
+                   })
+                  }
+                    
+                </tbody>
+              </table>
+            </section>
+                           
+          </main>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default MantenimientoUsuario;
