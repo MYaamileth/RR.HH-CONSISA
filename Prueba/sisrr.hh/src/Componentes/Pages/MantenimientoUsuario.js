8a9321d339aa79ac0d28a691e55d914c4d60fa28
DanielPineda19 <123615562+DanielPineda19@users.noreply.github.com>
2024-03-10 00:01:57 -0600

    Mantenimiento de Usuario P2
    Se modifico el mantenimiento de Usuario

--- Prueba/sisrr.hh/src/Componentes/Pages/MantenimientoUsuario.js
+++ Prueba/sisrr.hh/src/Componentes/Pages/MantenimientoUsuario.js
@@ -7,7 +7,6 @@ import Swal from 'sweetalert2';
 
 const MantenimientoUsuario = ({ onClose }) => {
   const [cargando, setCargando] = useState(false);
-  
    //Trae valores capturados por los campos del frontend
    useEffect(() => {
     const traerUsuarios = async () => {
@@ -29,12 +28,12 @@ const MantenimientoUsuario = ({ onClose }) => {
             <input
               type="text"
               placeholder="Usuario"
-              class="textbox"
+              class="textbox custom-input"
             />
             <input
               type="text"
               placeholder="Nombre Completo"
-              class="textbox"
+              class="textbox custom-input"
             />
           </div>
     
@@ -43,41 +42,42 @@ const MantenimientoUsuario = ({ onClose }) => {
               <input
                 type="text"
                 placeholder="Contraseña"
-                class="textbox"
+                class="textbox custom-input"
               />
               <input
                 type="text"
                 placeholder="Email"
-                class="textbox"
+                class="textbox custom-input"
               />
-              <div class="checkbox-container">
-                <div class="estado-container">
-                  <label for="estado_usuario">Estado: </label>
-                  <input 
-                    type="checkbox"
-                    id="estado_usuario"
-                    class="input"
-                  />
-                </div>
-                <span class="estado-label">Activo</span>
-              </div>
             </div>
           </div>
     
+
           <div class="input-container">
+            <div class="checkbox-container">
+                  <div class="estado-container">
+                    <input 
+                      type="checkbox"
+                      id="estado_usuario"
+                      class="input"
+                    />
+                  </div>
+                  <span class="estado-label">Activo</span>
+            </div>
+
             <div class="flex-row">
-              <label>
+              <label  class="custom-label">
                 Puesto:
-                <select class="inputPuesto">
+                <select class="inputPuesto custom-select">
                   <option value="administrador">Gerente de IT</option>
                   <option value="usuario">Administrador de BD</option>
                   <option value="agregarRol">Agregar Rol</option>
                 </select>
               </label>
     
-              <label>
+              <label  class="custom-label">
                 Rol:
-                <select class="inputRol">
+                <select class="inputRol custom-select">
                   <option value="administrador">Administrador</option>
                   <option value="usuario">Usuario</option>
                   <option value="agregarRol">Agregar Rol</option>
@@ -88,7 +88,7 @@ const MantenimientoUsuario = ({ onClose }) => {
         `,
         showCancelButton: true,
         showConfirmButton: true,
-        confirmButtonColor: '#3085d6',
+        confirmButtonColor: '#7300f7',
         cancelButtonColor: '#d33',
         confirmButtonText: 'Crear',
         cancelButtonText: 'Cancelar',
@@ -108,16 +108,16 @@ const MantenimientoUsuario = ({ onClose }) => {
       Swal.fire({
         title: 'Editar Usuario',
         html: `
-          
+            <div class="input-container">
             <input
               type="text"
               placeholder="Usuario"
-              class="textbox"
+              class="textbox custom-input"
             />
             <input
               type="text"
               placeholder="Nombre Completo"
-              class="textbox"
+              class="textbox custom-input"
             />
           </div>
     
@@ -126,41 +126,42 @@ const MantenimientoUsuario = ({ onClose }) => {
               <input
                 type="text"
                 placeholder="Contraseña"
-                class="textbox"
+                class="textbox custom-input"
               />
               <input
                 type="text"
                 placeholder="Email"
-                class="textbox"
+                class="textbox custom-input"
               />
-              <div class="checkbox-container">
-                <div class="estado-container">
-                  <label for="estado_usuario">Estado: </label>
-                  <input 
-                    type="checkbox"
-                    id="estado_usuario"
-                    class="input"
-                  />
-                </div>
-                <span class="estado-label">Activo</span>
-              </div>
             </div>
           </div>
     
+
           <div class="input-container">
+            <div class="checkbox-container">
+                  <div class="estado-container">
+                    <input 
+                      type="checkbox"
+                      id="estado_usuario"
+                      class="input"
+                    />
+                  </div>
+                  <span class="estado-label">Activo</span>
+            </div>
+
             <div class="flex-row">
-              <label>
+              <label  class="custom-label">
                 Puesto:
-                <select class="inputPuesto">
+                <select class="inputPuesto custom-select">
                   <option value="administrador">Gerente de IT</option>
                   <option value="usuario">Administrador de BD</option>
                   <option value="agregarRol">Agregar Rol</option>
                 </select>
               </label>
     
-              <label>
+              <label  class="custom-label">
                 Rol:
-                <select class="inputRol">
+                <select class="inputRol custom-select">
                   <option value="administrador">Administrador</option>
                   <option value="usuario">Usuario</option>
                   <option value="agregarRol">Agregar Rol</option>
@@ -171,13 +172,13 @@ const MantenimientoUsuario = ({ onClose }) => {
         `,
         showCancelButton: true,
         showConfirmButton: true,
-        confirmButtonColor: '#3085d6',
+        confirmButtonColor: '#7300f7',
         cancelButtonColor: '#d33',
         confirmButtonText: 'Guardar',
         cancelButtonText: 'Cancelar',
       }).then(response => {
         if(response.isConfirmed){
-          //Función para el envio de datos al metodo para editar de la Api.
+          //Función para el evio de datos al metodo para editar de la Api.
           //EditarUsuario();
           Swal.fire('Usuario editado con éxito');
         }else if(response.isDenied){
@@ -187,14 +188,14 @@ const MantenimientoUsuario = ({ onClose }) => {
     };
 
 
-  //Pantalla eliminar usuario
+//Pantalla eliminar usuario
     const pantallaEliminar = () => {
       Swal.fire({
         title: 'Eliminar Usuario',
-        text: '¿Está seguro de eliminar este usuario?',
+        text: '¿Estás seguro de que deseas eliminar este usuario?',
         icon: 'warning',
         showCancelButton: true,
-        confirmButtonColor: '#3085d6',
+        confirmButtonColor: '#7300f7',
         cancelButtonColor: '#d33',
         confirmButtonText: 'Sí, eliminar',
         cancelButtonText: 'Cancelar'
@@ -202,7 +203,7 @@ const MantenimientoUsuario = ({ onClose }) => {
         if (result.isConfirmed) {
           // Aquí puedes llamar a la función para eliminar el usuario
           // eliminarUsuario();
-          Swal.fire('¡Eliminado!', 'Usuario Eliminado con Éxito', 'success');
+          Swal.fire('Eliminado!', 'El usuario ha sido eliminado.', 'success');
         }
       });
     };
@@ -248,29 +249,38 @@ const MantenimientoUsuario = ({ onClose }) => {
   // const res = await axios.get("http://localhost:8800/Usuario")
 
   // Variable de estado para indicar si se está cargando la información
-  const [idusuario, setIdUsuario] = useState("");
-  const [estado, setEstado] = useState("");
-  const [roles, setRoles] = useState("");
-  const [puesto, setPuesto] = useState("");
-  const [usuario, setUsuario] = useState("");
-  const [nombreCompletoUsuario, setNombreCompletoUsuario] = useState("");
-  const [contraseña, setContraseña] = useState("");
-  const [fecha_Ultima_Conexion, setFecha_Ultima_Conexion] = useState("");
-  const [primer_Ingreso, setPimerIngreso] = useState("");
-  const [correo_Electronico, setCorreo_Electronico] = useState("");
-  const [fecha_Vencimiento, setFecha_Vencimiento] = useState("");
-  const [creado_Por, setCreado_Por] = useState("");
-  const [modificado_Por, setModificado_Por] = useState("");
-  const [fecha_Creacion, setFecha_Creacion] = useState("");
-  const [fecha_Modificacion, setFecha_Modificacion] = useState("");
+      const [idusuario, setIdUsuario] = useState("");
+      const [estado, setEstado] = useState("");
+      const [roles, setRoles] = useState("");
+      const [puesto, setPuesto] = useState("");
+      const [usuario, setUsuario] = useState("");
+      const [nombreCompletoUsuario, setNombreCompletoUsuario] = useState("");
+      const [contraseña, setContraseña] = useState("");
+      const [fecha_Ultima_Conexion, setFecha_Ultima_Conexion] = useState("");
+      const [primer_Ingreso, setPimerIngreso] = useState("");
+      const [correo_Electronico, setCorreo_Electronico] = useState("");
+      const [fecha_Vencimiento, setFecha_Vencimiento] = useState("");
+      const [creado_Por, setCreado_Por] = useState("");
+      const [modificado_Por, setModificado_Por] = useState("");
+      const [fecha_Creacion, setFecha_Creacion] = useState("");
+      const [fecha_Modificacion, setFecha_Modificacion] = useState("");
   
-
-
-  const [usuarioLista, setLista]=useState([]);//Lita para trer usuarios
-
+      const [usuarioLista, setLista]=useState([]);//Lita para trer usuarios
 
 
   //EMPIEZA CRUD
+
+      const [busqueda, setBusqueda] = useState("");
+
+      // Filtro de usuarios basado en la busqueda
+      const listaFiltrada = usuarioLista.filter((usuario) => {
+        return (
+          usuario.Usuario.toLowerCase().includes(busqueda.toLowerCase()) ||
+          usuario.Nombre_Completo_Usuario.toLowerCase().includes(
+            busqueda.toLowerCase()
+          )
+        );
+      });
 
   //Envio de valores capturados por los campos del frontend
   const creacionUsuario =()=>{
@@ -292,6 +302,7 @@ const MantenimientoUsuario = ({ onClose }) => {
       Modificado_por: modificado_Por,
       Fecha_creacion: fecha_Creacion,
       Fecha_modificacion: fecha_Modificacion
+    
     }). then(()=> {
       //traerUsuarios(); //cada que se guarde un usuario se enlistara en la tabla
       
@@ -299,71 +310,78 @@ const MantenimientoUsuario = ({ onClose }) => {
     });
   }
 
- 
-
   //traerUsuarios(); //Desde que se ingresa al apartado, traera los usuarios
 
-//EN LOS CAMPOS DE USUARIO Y EMAIL, CA,BIAR EL SET UNA VEZ QUE FUNIONE, PUES ESTAN LOS DE PRUEBA
+//EN LOS CAMPOS DE USUARIO Y EMAIL, CAMBIAR EL SET UNA VEZ QUE FUNIONE, PUES ESTAN LOS DE PRUEBA
   return (
     <div data-maintenance-usuario="true" className="maintenance-container">
       {cargando ? (
         <div className="loading">Cargando...</div>
       ) : (
         <>
-          <main>
-            <section className="formulario">
-              <div className="form-container">
+          <section className="formulario">
+            <h1 className="mr-5 ml-5 mt-5">MANTENIMIENTO DE USUARIOS</h1>
+            <p></p>
+            
+            <div className="form-buttons">
+                  <div className="search-container">
+                      <i className='bx bx-search icon'></i>
+                      <input
+                        type="text"
+                        placeholder="Buscar por Usuario o Nombre Completo"
+                        className="search-input"
+                        value={busqueda}
+                        onChange={(e) => setBusqueda(e.target.value)}
+                      />
+                  </div>
+                  <button onClick={pantallaCrear} className="submit">Crear Usuario</button>
+                  <button className="submit"> Generar Informe</button>
+            </div>
 
-              <button onClick={cerrarComponente} className="submit" id="Salir">X</button>
-                <h1>MANTENIMIENTO DE USUARIOS</h1>
-               
-                <p></p>
-                
-                <div className="form-buttons">
-                  <button onClick={pantallaCrear} className="submit">Crear</button>
-                  <button onClick={pantallaEditar} className="submit">Editar</button>
-                  <button onClick={pantallaEliminar} className="submit">Eliminar</button>
-                              
-                </div>
-              </div>
-            </section>
             <section className="tabla-usuarios">
-              <table className="table table-striped table-hover">
+              <table className="table table-hover table-bordered" id="tblUsuarios">
                 <thead>
                   <tr>
-                    <th>ID Usuario</th>
                     <th>Usuario</th>
                     <th>Nombre Completo de Usuario</th>
-                    <th>Contraseña</th>
                     <th>Fecha Vencimiento De Contraseña</th>
+                    <th>Correo Electronico</th>
+                    <th>Acciones</th>
                   </tr>
                 </thead>
                 <tbody>
-                {usuarioLista.map((val, key) => {
-                    return   <tr>
-                            <th>{val.Id_Estado}</th>
-                            <th>{val.Id_rol}</th>
-                            <th>{val.Id_puesto}</th>
+                {listaFiltrada.length > 0 ? (
+                  usuarioLista.map((val, key) => {
+                    return (<tr>
                             <th>{val.Usuario}</th>
                             <th>{val.Nombre_Completo_Usuario}</th>
-                            <th>{val.Contraseña}</th>
-                            <th>{val.Fecha_Ultima_Conexion}</th>
-                            <th>{val.Primer_ingreso}</th>
                             <th>{val.Correo_electronico}</th>
-                            <th>{val.Fecha_vencimiento}</th>
-                            <th>{val.Creado_por}</th>
-                            <th>{val.Modificado_por}</th>
-                            <th>{val.Fecha_creacion}</th>
-                            <th>{val.Fecha_modificacion}</th>
+                            <th>{val.Fecha_Ultima_Conexion}</th>
+                              <td> 
+                                <button onClick={pantallaEditar} className="submit icon-button"> Editar </button>
+                                <button onClick={pantallaEliminar} className="submit icon-button"> Eliminar </button>
+                               </td>
                             </tr>
+                    );
                    })
-                  }
+                  
+                ) : (
+                  <tr>
+                    <td colSpan="5">
+                      <p className="text-center">No se encontraron resultados</p>
+                    </td>
+                  </tr>
+                  
+                )}
                     
                 </tbody>
               </table>
-            </section>
-                           
-          </main>
+
+              </section> 
+
+              <script src="./Paginador.js"></script>
+
+          </section>
         </>
       )}
     </div>
